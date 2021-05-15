dcfcd4e7897d76f25ee8a0f3e47816419e7195a5
elykamaya <72068131+elykamaya@users.noreply.github.com>
2021-05-15 19:02:31 +0700

    home.php

---
+++ home.php
@@ -0,0 +1,20 @@
+<?php
+  include 'nav.php';
+?>
+
+<div class="container">
+    <h3>Selamat datang Web Ini</h3>
+    <p>Ini adalah web untuk memenuhi Quiz 2 pemrograman berbasis website.</p>
+
+    <h4>IDentitas</h4>
+    <ol>
+        <li>Nama : Elyka Maya Ningtiyas</li>
+        <li>NIM : 192410101107</li>
+        <li>Kelas : Pemrograman Berbasis Web A</li>
+        <li>Menggunakan Session dan Cookie</li>
+    </ol>
+</div>
+
+<?php
+    include 'footer.php';
+?>
