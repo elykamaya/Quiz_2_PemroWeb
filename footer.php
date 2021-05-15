3d8755bf4b35687376c7c3e85d7433a7412522c1
elykamaya <72068131+elykamaya@users.noreply.github.com>
2021-05-15 19:03:02 +0700

    footer.php

---
+++ footer.php
@@ -0,0 +1,20 @@
+
+<style>
+.footer {
+  position: fixed;
+  bottom: 0;
+}
+</style>
+
+<div class="footer">
+<footer class="page-footer blue" >
+    
+    <div class="footer-copyright text-center py-3">
+    © 2021 Copyright: Elyka Maya Ningtiyas - 192410101107 </br>
+    Pemrograman Berbasis Web - A -
+    </div>
+    
+</footer>
+
+</div>
+</html>
