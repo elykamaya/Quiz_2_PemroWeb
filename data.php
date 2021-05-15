23a2d1b6be55c28dd9a951fcbdff5175bcc82b91
elykamaya <72068131+elykamaya@users.noreply.github.com>
2021-05-15 19:03:10 +0700

    data.php

---
+++ data.php
@@ -0,0 +1,24 @@
+<?php
+
+include "nav.php";
+
+?>
+
+<div class="container">
+    <div class="table-responsive">
+        <h3>Data</h3>  
+        <tbody>
+        <?php
+            echo "INI TIDAK ADA ISI NYA";
+        ?>
+        <h5>Elyka Maya Ningtiyas</h5>
+        <h5>192410101107</h5>
+        <h5>Pemro Web A</h5>
+        </tbody>         
+    </div>
+</div>
+
+
+<?php
+    include "footer.php";
+?>
